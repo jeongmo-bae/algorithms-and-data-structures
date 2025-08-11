0c7aceeb08fc61c77732d646f4494044fe05013e
jeongmo-bae <jungmob1024@gmail.com>
2025-08-11 23:13:25 +0000

    solve programmers 42626

--- java/src/com/programmers/essential/heap/Lessons_42626.java
+++ java/src/com/programmers/essential/heap/Lessons_42626.java
@@ -1,8 +1,34 @@
 package com.programmers.essential.heap;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Lessons_42626 {
     public int solution(int[] scoville, int K) {
         int answer = 0;
+        ArrayList<Integer> arrayListScoville = new ArrayList<>();
+        for ( Integer scov : scoville) {
+            arrayListScoville.add(scov);
+        }
+        while (arrayListScoville.size() > 1){
+            answer += 1;
+            Collections.sort(arrayListScoville);
+            Integer first = arrayListScoville.remove(0);
+            Integer second = arrayListScoville.remove(0);
+            Integer nextScovil = first + 2*second ;
+            arrayListScoville.add(0,nextScovil);
+            boolean allUpperThanK = true;
+            for (Integer scov : arrayListScoville) {
+                if (scov < K){
+                    allUpperThanK = false;
+                    break;
+                }
+            }
+            if (allUpperThanK){
+                break;
+            }
+        }
+        //섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
         return answer;
     }
 }
---
+++ java/src/com/programmers/essential/heap/Main.java
@@ -0,0 +1,10 @@
+package com.programmers.essential.heap;
+
+public class Main {
+    public static void main(String[] args) {
+        Lessons_42626 sol = new Lessons_42626();
+        int answer = sol.solution(new int[]{1, 2, 3, 9, 10, 12}, 7);
+        System.out.println("==================");
+        System.out.println(answer);
+    }
+}
