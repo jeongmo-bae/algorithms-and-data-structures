12783de04be1d2f0289e54f6748deb7c9e60e9bf
jeongmo-bae <jungmob1024@gmail.com>
2025-08-12 12:48:47 +0000

    solve programmers 42626

--- java/src/com/programmers/essential/heap/Lessons_42626.java
+++ java/src/com/programmers/essential/heap/Lessons_42626.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Lessons_42626 {
+    public static void main(String[] args) {
+        Lessons_42626 sol = new Lessons_42626();
+        int answer = sol.solution(new int[]{1, 2, 3, 9, 10, 12}, 7);
+        System.out.println("==================");
+        System.out.println(answer);
+        System.out.println("==================");
+    }
     public int solution(int[] scoville, int K) {
         int answer = 0;
         ArrayList<Integer> arrayListScoville = new ArrayList<>();
