552615d211be7a0b5e9b426bca942c21b4ddf5e4
배정모 <jungmo@baejeongmoui-Macmini.local>
2025-08-18 00:49:07 +0900

    feat : solve programmers 42748

--- java/src/com/programmers/essential/sort/Lessons_42748.java
+++ java/src/com/programmers/essential/sort/Lessons_42748.java
@@ -1,8 +1,28 @@
 package com.programmers.essential.sort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Lessons_42748 {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            answer[i] = preprocess(array,commands[i]).get(commands[i][2]-1);
+        }
+        System.out.println(Arrays.toString(answer));
         return answer;
+    }
+    public static ArrayList<Integer> preprocess(int[] array, int[] command){
+        ArrayList<Integer> preprocessedArray =  new ArrayList<>();
+        for (int i = command[0]-1; i < command[1]; i++) {
+            preprocessedArray.add(array[i]);
+        }
+        Collections.sort(preprocessedArray);
+        return preprocessedArray;
+    }
+    public static void main(String[] args) {
+        Lessons_42748 sol = new Lessons_42748();
+        sol.solution(new int[]{1,5,2,6,3,7,4},new int[][]{{2,5,3},{4,4,1},{1,7,3}});
     }
 }
