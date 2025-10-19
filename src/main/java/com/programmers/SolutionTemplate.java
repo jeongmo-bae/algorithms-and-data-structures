cfb832ea550e4e80f92ccb1413e206e19f7053b2
배정모 <jungmo@baejeongmoui-Macmini.local>
2025-10-20 00:30:06 +0900

    feat : fix SolutionTemplate Interface

--- src/main/java/com/programmers/SolutionTemplate.java
+++ src/main/java/com/programmers/SolutionTemplate.java
@@ -1,5 +1,9 @@
 package com.programmers;
 @FunctionalInterface
 public interface SolutionTemplate<I, O> {
+    public static void print(Object obj) {
+        System.out.println(obj);
+    };
+
     O solution(I input);
 }
