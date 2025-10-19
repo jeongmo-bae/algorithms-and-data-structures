d5c30711fa7dfc4fb49ef96d3108e14f9f93af7f
배정모 <jungmo@baejeongmoui-Macmini.local>
2025-10-19 13:49:26 +0900

    feat : solve https://school.programmers.co.kr/learn/courses/30/lessons/42577

--- src/main/java/com/programmers/essential/hash/Lesson_42577.java
+++ src/main/java/com/programmers/essential/hash/Lesson_42577.java
@@ -1,8 +1,19 @@
 package com.programmers.essential.hash;
 
+import java.util.Arrays;
+
 public class Lesson_42577 {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
+        Arrays.sort(phone_book);
+        System.out.println("Arrays.toString(phone_book) = " + Arrays.toString(phone_book));
+
+        for (int i = 0; i < phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
+        }
         return answer;
     }
 }
--- src/test/java/com/programmers/essential/hash/Lesson_42577Test.java
+++ src/test/java/com/programmers/essential/hash/Lesson_42577Test.java
@@ -1,7 +1,28 @@
 package com.programmers.essential.hash;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Lesson_42577Test {
+
+    @Test
+    @DisplayName("validate solution method with test case")
+    void testSolution(){
+        // given
+        Lesson_42577 lesson = new Lesson_42577();
+
+        // when
+        String[] phoneBook1 = {"119", "97674223", "1195524421"};
+        String[] phoneBook2 = {"123","456","789"};
+        String[] phoneBook3 = {"12","123","1235","567","88"};
+
+        // then
+        Assertions.assertThat(lesson.solution(phoneBook1)).isEqualTo(false);
+        Assertions.assertThat(lesson.solution(phoneBook2)).isEqualTo(true);
+        Assertions.assertThat(lesson.solution(phoneBook3)).isEqualTo(false);
+    }
 
 }
